ca532e9b369ecb5cb11634588441eebc08afcab0
MilenaGkm <milenagaranin@gmail.com>
2021-07-15 20:05:33 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react"
+import Users from './components/UsersComponent';
+import './App.css';
 
 function App() {
-    return (<div>
-        <h2>Admin user app</h2>
-        <h3>Date : {new Date().toDateString()}</h3>
-    </div>)
+    return (
+        <div>
+            <h2>Admin user app</h2>
+            <h3>Date : {new Date().toDateString()}</h3>
+            <Users />
+        </div>
+    )
 }
 
 export default App
